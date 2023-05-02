86e4a7da3e42b199fbfa9d22569e3d0a8589a1c2
charlie123446 <7eleven123446@gmail.com>
2023-05-02 23:17:11 +0800

    write my name

--- k線圖顯示.py
+++ k線圖顯示.py
@@ -72,7 +72,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "C109151142 陳明凱"))
         self.label_2.setText(_translate("MainWindow", "請輸入股票代號:"))
         self.pushButton.setText(_translate("MainWindow", "顯示K線圖"))
         self.label_3.setText(_translate("MainWindow", "顯示解果文字:"))
