31faf4b9d5d995453bfbd11a51e713add4705444
charlie123446 <61088674+charlie123446@users.noreply.github.com>
2023-05-05 01:12:33 +0800

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import MA_strategy
-import yF_Kbar
-if __name__ == '__main__':
-    stock_id= 1432
-    df = MA_strategy.get_MA(stock_id)
-    MA_strategy.trade(df)
-    df.to_excel(str(stock_id)+".xlsx")
